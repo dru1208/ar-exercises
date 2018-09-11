39182c2238a4b0ef7928381cd278469a8848f14f
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-09-11 17:16:22 +0000

    finished exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+puts "#{@store1[:name]} is the name of @store1"
+puts "#{@store2[:name]} is the name of @store2"
+
+@store1.update(name: 'New York')
+puts "#{@store1[:name]} is the new name of @store1"
